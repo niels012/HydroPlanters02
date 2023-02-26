3792355db432b522830ed8511d96faaf55e043c5
Nilo <urmenetanilo@gmail.com>
2023-02-26 14:54:56 +0800

    first commit

---
+++ hydro_app/resources/views/partials/loginlogo.blade.php
@@ -0,0 +1,5 @@
+<div class="login-logo">
+    <a href="../../index2.html">
+        <img src="images/HPlogobig.png" height="130px" alt="logo">
+    </a>    
+</div
---
+++ hydro_app/resources/views/signuplte.blade.php
@@ -0,0 +1,121 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Sign up</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="../../assets/plugins/fontawesome-free/css/all.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="../../assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="../../assets/dist/css/adminlte.min.css">
+</head>
+<div class="container">
+    @include('partials.messages')
+</div>
+<body class="hold-transition register-page">
+    <div class="login-box">
+        @include('partials.loginlogo')
+    </div>
+  <div class="card">
+    <div class="card-body register-card-body">
+        <h3>Create Account </h3>
+      {{-- <p class="login-box-msg">Register a new membership</p> --}}
+
+      <form method="POST" action="{{ route('register') }}">
+        @csrf
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" name="first_name" placeholder="First name">
+          {{-- <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user"></span>
+            </div>
+          </div> --}}
+            <input type="text" class="form-control" name="middle_name" placeholder="Middle name">
+            {{-- <div class="input-group-append"> --}}
+              <div class="input-group-text">
+                <span class="fas fa-user"></span>
+              </div>
+            {{-- </div> --}}
+        </div>
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" name="last_name" placeholder="Last name">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-user"></span>
+              </div>
+            </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="email" class="form-control" name="email" placeholder="Email">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" name="password" placeholder="Password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        {{-- <div class="input-group mb-3">
+          <input type="password" class="form-control" placeholder="Retype password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>    
+        </div> --}}
+        <div class="row">
+          {{-- <div class="col-8">
+            <div class="icheck-primary">
+              <input type="checkbox" id="agreeTerms" name="terms" value="agree">
+              <label for="agreeTerms">
+               I agree to the <a href="#">terms</a>
+              </label>
+            </div>
+          </div> --}}
+          <!-- /.col -->
+          <div class="col-12">
+            <button type="submit" class="btn btn-primary btn-block mb-2">Register</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+
+      {{-- <div class="social-auth-links text-center">
+        <p>- OR -</p>
+        <a href="#" class="btn btn-block btn-primary">
+          <i class="fab fa-facebook mr-2"></i>
+          Sign up using Facebook
+        </a>
+        <a href="#" class="btn btn-block btn-danger">
+          <i class="fab fa-google-plus mr-2"></i>
+          Sign up using Google+
+        </a>
+      </div> --}}
+
+      <a href="{{ route('userlogin') }}" class="text-center">I already have an account</a>
+    </div>
+    <!-- /.form-box -->
+  </div><!-- /.card -->
+</div>
+<!-- /.register-box -->
+
+<!-- jQuery -->
+<script src="../../assets/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../../assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../../assets/dist/js/adminlte.min.js"></script>
+</body>
+</html>
