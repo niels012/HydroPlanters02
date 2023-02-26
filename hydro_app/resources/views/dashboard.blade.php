9fadbcd51c89dd4b9d7850aae928ac55f5178680
Nilo <urmenetanilo@gmail.com>
2023-02-26 22:23:08 +0800

    new laravel app

--- hydro_app/resources/views/dashboard.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.default')
-
-@section('content')
-
-  <h1>Welcome to your dashboard {{Auth::user()->name;}}!</h1>
-
-@endsection
--- hydro_app/resources/views/partials/loginlogo.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="login-logo">
-    <a href="../../index2.html">
-        <img src="images/HPlogobig.png" height="130px" alt="logo">
-    </a>    
-</div
