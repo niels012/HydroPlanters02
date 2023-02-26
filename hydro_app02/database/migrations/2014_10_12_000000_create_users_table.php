6e96181f6d5274e32c84ef0bc9f40af51fdb2c8b
Nilo <urmenetanilo@gmail.com>
2023-02-27 00:40:09 +0800

    login page added

--- hydro_app02/database/migrations/2014_10_12_000000_create_users_table.php
+++ hydro_app02/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->boolean('is_admin');
+            $table->string('firstame');
+            $table->string('middleame');
+            $table->string('lastame');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
--- hydro_app02/routes/web.php
+++ hydro_app02/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('dashboard', \App\Http\Controllers\UserController::class);
+
+Route::get('login', [\App\Http\Controllers\AuthController::class, 'login'])->name('login');
